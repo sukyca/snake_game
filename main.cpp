2c49a90042198774e9e5f7d8bb95febc0257cab7
Antonio Suljić <antonio.suljic1@gmail.com>
2022-03-10 22:27:24 +0100

    Create class Point with constructors

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <conio.h>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Point
+{
+private:
+    int x;
+    int y;
+
+public:
+    Point()
+    {
+        x = y = 10;
+    }
+
+    Point(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+};
 
 int main()
 {
