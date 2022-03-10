171edf5dd7c74cfcb423ce3f36776eff3c3eb384
Antonio Suljić <antonio.suljic1@gmail.com>
2022-03-11 00:09:48 +0100

    Create Move method and input char

--- main.cpp
+++ main.cpp
@@ -62,6 +62,12 @@ public:
         x++;
     }
 
+    void CopyPos(Point *p)
+    {
+        p->x = x;
+        p->y = y;
+    }
+
     void Draw()
     {
         gotoxy(x, y);
@@ -77,6 +83,74 @@ public:
     void Debug()
     {
         cout << " (" << x << ", " << y << ") ";
+    }
+};
+
+class Snake
+{
+private:
+    Point *cell[MAXSNAKESIZE];
+    int size;
+    char dir;
+
+public:
+    Snake()
+    {
+        size = 1;
+        cell[0] = new Point(20, 20);
+        for (int i = 1; i < MAXSNAKESIZE; i++)
+        {
+            cell[i] = NULL;
+        }
+    }
+
+    void addCell(int x, int y)
+    {
+        cell[size++] = new Point(x, y);
+    }
+
+    void turnUp()
+    {
+        dir = 'w';
+    }
+
+    void turnDown()
+    {
+        dir = 's';
+    }
+
+    void turnLeft()
+    {
+        dir = 'a';
+    }
+
+    void turnRight()
+    {
+        dir = 'd';
+    }
+
+    void Move()
+    {
+        for (int i = size - 1; i < 0; i--)
+        {
+            cell[i]->CopyPos(cell[i - 1]);
+        }
+
+        switch (dir)
+        {
+        case 'w':
+            cell[0]->moveUp();
+            break;
+        case 's':
+            cell[0]->moveDown();
+            break;
+        case 'a':
+            cell[0]->moveLeft();
+            break;
+        case 'd':
+            cell[0]->moveRight();
+            break;
+        }
     }
 };
 
